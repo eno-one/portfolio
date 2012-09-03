90ac348b3dcd38e19208c8cefe39b265421ba785
ulya <ulya@vsyako.ru>
2012-09-03 21:07:55 +0400

    из дома

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
   $('dt').click(function(){
-    $(this).next('dd').slideToggle(200).toggleClass('opened');
+    var $dt = $(this),
+        $dd = $dt.next('dd');
+        
+    
+    
+    $dd.slideToggle(200, function() {
+     //   var topPos = $dt.offset().top - $(window).scrollTop();
+     //   window.scrollBy(0, topPos);
+        
+      }).toggleClass('opened');
   });
   
+  
   $('.show-all').click(function(){
-    $(this).hide();
-    $('#cl').show();
     $('dd').not('.opened').slideDown(200);
     return false;
   });
+  
 
   
 });
