17b89fcf92068aff7e89daa1ef3b852f0551619a
ulya <ulya@vsyako.ru>
2012-09-03 22:08:07 +0400

    доработки

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
+  
   $('dt').click(function(){
     var $dt = $(this),
         $dd = $dt.next('dd');
-        
     
     
     $dd.slideToggle(200, function() {
      //   var topPos = $dt.offset().top - $(window).scrollTop();
      //   window.scrollBy(0, topPos);
-        
       }).toggleClass('opened');
   });
   
