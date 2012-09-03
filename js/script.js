5630d2c628cbd1384c6da07528e6c02c79946d96
ulya <ulya@vsyako.ru>
2012-09-03 22:09:40 +0400

    убрала лишнее из js

--- js/script.js
+++ js/script.js
@@ -4,19 +4,11 @@ $(document).ready(function(){
     var $dt = $(this),
         $dd = $dt.next('dd');
     
-    
-    $dd.slideToggle(200, function() {
-     //   var topPos = $dt.offset().top - $(window).scrollTop();
-     //   window.scrollBy(0, topPos);
-      }).toggleClass('opened');
+    $dd.slideToggle(200).toggleClass('opened');
   });
-  
   
   $('.show-all').click(function(){
     $('dd').not('.opened').slideDown(200);
     return false;
   });
-  
-
-  
 });
