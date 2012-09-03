248f4cde7a908f05c540b7b0adb5082100b0a199
ulya <ulya@vsyako.ru>
2012-09-03 16:38:59 +0400

    доработки

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 $(document).ready(function(){
   $('dt').click(function(){
-    //$(this).next('dd').slideToggle(200).toggleClass('opened');
-    $(this).next('dd').css('display', 'block').toggleClass('opened');
+    $(this).next('dd').slideToggle(200).toggleClass('opened');
   });
   
-  $('.showAll').click(function(){
-    $('#op').toggle();
-    $('#cl').toggle();
+  $('.show-all').click(function(){
+    $(this).hide();
+    $('#cl').show();
     $('dd').not('.opened').slideDown(200);
     return false;
   });
-  
-  $('#cl').click(function(){
-    $('.opened').slideUp(200);
-  })
 
   
 });
